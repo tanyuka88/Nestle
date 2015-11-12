7b05090b7574dc5937ae47a3788e3c8723c12607
tanya <tatyana.chen.88@gmail.com>
2015-11-12 17:04:48 +0500

    adaptive

--- js/js.js
+++ js/js.js
@@ -6,8 +6,14 @@ jQuery(document).ready(function($){
     });
 
     $("header .top-menu .main-top-menu > li").click(function(){
-        $(this).find("ul").toggle();
+        if ($("header .top-menu .main-top-menu > li").hasClass("tab-white")){
+            $("header .top-menu .main-top-menu > li").removeClass("tab-white").find("ul").css("display","none");
+            $("header .top-menu .main-top-menu > li").find(">a").removeClass("blue");
+        }
+        $(this).addClass("tab-white").find("ul").toggle();
+        $(this).find(">a").addClass("blue");
     });
+
     $(".close").click(function(){
         $(".top-menu").toggle();
     })
